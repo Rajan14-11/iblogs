f178452cd4801aade6caff6ca6117e78d509642f
Rajan14-11 <90331294+Rajan14-11@users.noreply.github.com>
2021-11-21 18:28:23 +0530

    Add files via upload

---
+++ models/Notes.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const NotesSchema = new Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  tag: { type: String, default: "General" },
+  image: {},
+});
+
+module.exports = mongoose.model("notes", NotesSchema);
